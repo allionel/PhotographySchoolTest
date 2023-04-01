2d5281d058af2497a16884cbe329c3318b94ce4d
“Ali-iOS” <alireza@coto.world>
2023-04-01 15:52:39 +0330

    Initial new project

---
+++ Photography/Photography/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Photography
+//
+//  Created by Alireza Sotoudeh on 4/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack { }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
