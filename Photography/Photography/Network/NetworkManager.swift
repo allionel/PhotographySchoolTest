65cf20beb2f39dce84b748d2e468da971189ec40
“Alireza” <alireza.sotoodeh.1@gmail.com>
2023-04-02 10:46:43 +0330

    Implement network manager

--- Photography/Photography/Network/NetworkManager.swift
+++ Photography/Photography/Network/NetworkManager.swift
@@ -5,4 +5,52 @@
 //  Created by Alireza Sotoudeh on 4/2/23.
 //
 
+import Foundation
 import Alamofire
+
+protocol APIClient {
+    func request<T>(_: URLRequestConvertible, result: @escaping (Result<T, NetworkError>) -> Void) where T: Decodable, T: Encodable
+}
+
+final class NetworkManager {
+    private let sessionManager: Session
+    private let decoder: JSONDecoder
+    
+    required init() {
+        let config = URLSessionConfiguration.af.default
+        config.timeoutIntervalForRequest = 60
+        config.timeoutIntervalForResource = 60
+        
+        sessionManager = .init(configuration: config)
+        decoder = .init()
+    }
+}
+
+extension NetworkManager: APIClient {
+    func request<T>(_ endpoint: Alamofire.URLRequestConvertible, result: @escaping (Result<T, NetworkError>) -> Void) where T : Decodable, T : Encodable {
+        guard let urlRequest = try? endpoint.asURLRequest() else {
+            result(.failure(NetworkError.invalidRequest))
+            return
+        }
+        let request = sessionManager.request(urlRequest)
+
+        #if DEBUG
+            print("REQUEST: \(debugPrint(request))")
+        #endif
+
+        request
+            .validate()
+            .responseDecodable(of: T.self) { response in
+                #if DEBUG
+                    print("Response: \(debugPrint(response))")
+                #endif
+                switch response.result {
+                case let .success(model):
+                    result(.success(model))
+                case let .failure(error):
+                    result(.failure(NetworkError(rawValue: error.responseCode ?? 1002) ?? NetworkError.unknown))
+                }
+            }
+    }
+    
+}
